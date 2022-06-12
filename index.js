bc8b050ce94b9724475090606a075b324a9e48c1
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2022-06-12 17:54:23 -0300

    delete trybe files

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use('/', router);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
-// não remova esse endpoint, e para o avaliador funcionar..
 app.get('/', (_request, response) => {
   response.send();
 });
